2c90cc813b38b9d4c16eb518436d1ca77a21744c
Daniel-Otieno <oderodanielotieno@gmail.com>
2022-04-08 17:13:42 +0300

    New commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
+*Simple C program that prints an output using puts*/
 #include <stdio.h>
+
 /**
- *main - entry point
+ *main - prints a string
  *
- *Return: 0 after printing the function
+ *Return: 0(Success)
  */
 int main(void)
 {
-puts("\"Programming is like buildiing a multilingual puzzle");
-return (0)
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
