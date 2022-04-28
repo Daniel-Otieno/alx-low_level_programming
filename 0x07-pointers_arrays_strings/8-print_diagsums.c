6372fd28e4d8b61b8ac75fc2e40b99d9be7f3008
Daniel-Otieno <oderodanielotieno@gmail.com>
2022-04-28 05:53:42 +0300

    New commit

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,45 +1,23 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- *function that prints the sum of the two diagonals of a square matrix of
- * integers
- *@a: input int array
- *@size: input int
- *
- *Return: void
+ * print_diagsums - prints the sum of the two
+ * diagonals of a square matrix of integers
+ * @a: input pointer.
+ * @size: size of the matrix
+ * Return: no return.
  */
-void print_diagsums(int *a, int size)/* a is a pointer to int*/
+void print_diagsums(int *a, int size)
 {
-	int sum = 0;
-	int i;
-	int j;
+	int i, sum1 = 0, sum2 = 0;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < (size * size); i++)
 	{
-		for (j = 0; j < size; j++)
-		{
-			if (i = j || (i + j == 4))
-				sum += a[i][j];
-		}
+		if (i % (size + 1) == 0)
+			sum1 += *(a + i);
+		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
+			sum2 += *(a + i);
 	}
-	printf("%d\n", sum);
+	printf("%d, %d\n", sum1, sum2);
 }
-int main(void)
-{
-    int c3[3][3] = {
-        {0, 1, 5},
-        {10, 11, 12},
-        {1000, 101, 102},
-    };
-    int c5[5][5] = {
-        {0, 1, 5, 12124, 1234},
-        {10, 11, 12, 123521, 12512},
-        {1000, 101, 102, 12545, 214543435},
-        {100, 1012451, 11102, 12545, 214543435},
-        {10, 12401, 10452, 11542545, 1214543435},
-    };
-    print_diagsums(c3, 3);
-    print_diagsums(c5, 5);
-    return (0);
-}
+
