7aa21ccf6fe522ba588d080c5576ecc0b271a23a
Daniel-Otieno <oderodanielotieno@gmail.com>
2022-05-14 22:37:00 +0300

    New commit

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef SIZE
+#define SIZE 1024
+
+/*
+* #ifndef _OBJECT_LIKE_MACRO_: Checks if -OBJECT_LIKE_MACRO*_ is defined
+*#define _OBJECT_LIKE_MACRO_ : Defines -OBJECT_LIKE_MACRO_
+*/
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,9 @@
+#ifndef PI
+
+/*
+* #ifndef PI : Checks if PI IS DEFINED
+* #define PI : Defines  PI  
+*/
+#define PI 3.14159265359
+
+#endif
